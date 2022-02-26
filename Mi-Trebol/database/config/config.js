f7d9ab8e4e038ae3fa4731271813345c98fba409
Rodrigo Bonka <rodrigobonka@MacBook-Air-de-Rodrigo-2.local>
2022-02-26 10:27:23 -0600

    Version 0.0.4 de MiTrebol

--- Mi-Trebol/database/config/config.js
+++ Mi-Trebol/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Daniel12&",
+    "password": "rootroot",
     "database": "mitrebol",
     "host": "127.0.0.1",
     "dialect": "mysql"
