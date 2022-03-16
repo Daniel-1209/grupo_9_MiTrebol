d0fa67687c5cda629e7565a6cabdfa72e9c49280
Harumi Camarillo <harumicamarillo@gmail.com>
2022-03-16 00:26:55 -0400

    validaciones en front end como UX con etiquetas small listas en edit y add product. Falta detalles

--- Mi-Trebol/public/js/validateAddProductFrontEnd.js
+++ Mi-Trebol/public/js/validateAddProductFrontEnd.js
@@ -1,66 +1,103 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     let formEdit = document.querySelector('#formEdit');
     let productName = document.querySelector('#text');
     let shortDescription = document.querySelector('#text2');
     let longDescription = document.querySelector('#text3');
     let imgMod = document.querySelector('#imgMod');
     let price = document.querySelector('#price');
-    let btnSubmit = document.querySelector('#submitEdit');
-    console.log(imgMod);
-    console.log(shortDescription);
-    console.log(btnSubmit);
+    let btnSubmitAdd = document.querySelector('#submitAdd');
+    let smallNameAdd = document.querySelector('.erNameAdd');
+    let smallShortAdd = document.querySelector('.erShortDesAdd');
+    let smallLongAdd = document.querySelector('.erLongDesAdd');
+    let smallImageAdd = document.querySelector('.erImageAdd');
+    let smallPriceAdd = document.querySelector('.erPriceAdd');
+    var errors = [];
 
-    productName.addEventListener('blur', function(){
-        alert('Este campo es obligatorio');
+    productName.addEventListener('change', function (){
+        if(productName.value == ""){
+            errors.push('Es necesario agregar el nombre del producto');
+            errors.forEach(error => {
+                if(error == 'Es necesario agregar el nombre del producto'){
+                   smallNameAdd.innerHTML += `${error}`
+                }
+            })
+            //alert('Es necesario agregar el nombre del producto');
+        };
+    })
+
+    shortDescription.addEventListener('change', function(){
+        if(shortDescription.value == ""){
+            errors.push('Es necesario escribir una descripción corta');
+            errors.forEach(error => {
+                if(error == 'Es necesario escribir una descripción corta'){
+                   smallShortAdd.innerHTML += `${error}`
+                    alert('Es necesario escribir una descripción corta');
+                }
+            })
+        }
+    })
+
+    longDescription.addEventListener('change', function(){
+        if(longDescription.value == ""){
+            errors.push('Agrega una descripción detallada del producto');
+            errors.forEach(error => {
+                if(error == 'Agrega una descripción detallada del producto'){
+                   smallLongAdd.innerHTML += `${error}`
+                    alert('Agrega una descripción detallada del producto');
+                }
+            })
+        }
+    })
+
+    imgMod.addEventListener('change', function () {
+        var filePath = this.value;
+        var allowedExtensions = /(.jpg||.jpeg||.png||.gif)$/i;
+        if (!allowedExtensions.exec(filePath)) {
+            errors.push('Extensión no permitida. Utiliza: .jpeg/.jpg/.png/.gif.');
+            errors.forEach(error => {
+                if(error == 'Extensión no permitida. Utiliza: .jpeg/.jpg/.png/.gif.'){
+                   smallImageAdd.innerHTML += `${error}`
+                    alert('Agrega una descripción detallada del producto');
+                }
+            })
+            fileInput.value = '';
+            return false;
+        } 
+        //else {  alert('Extensión correcta.');
+        //  return true; }
+    })
+
+    price.addEventListener('blur', function(){
+        errors.push('Debe asignar el precio');
+        errors.forEach(error => {
+            if(error == 'Debe asignar el precio'){
+               smallPriceAdd.innerHTML += `${error}`
+                alert('Debe asignar el precio');
+            }
         })
-        shortDescription.onBlur = () => {
-            this.alert('Este campo es obligatorio');
+    })
+
+    btnSubmitAdd.addEventListener('click', function (event) {
+        event.preventDefault();
+       
+        if (productName.value.length < 5) {
+            productName.alert('El nombre debe tener al menos 5 carácteres');
+            errors.productName = 'El nombre debe tener al menos 5 carácteres';
         }
-        longDescription.onBlur = () => {
-            this.alert('Este campo es obligatorio');
+        else if (shortDescription.value.length < 10) {
+            shortDescription.alert('La descripción corta debe tener mínimo 10 carácteres');
+            errors.shortDescription = ('La descripción corta debe tener mínimo 10 carácteres');
         }
-        imgMod.onBlur = () => {
-            this.alert('Es necesario añadir una imagen');
+        else if (longDescription.value.length < 20) {
+            longDescription.alert('La descripción larga debe tener mínimo 20 carácteres');
+            errors.longDescription = ('La descripción larga debe tener mínimo 20 carácteres');
         }
-        imgMod.addEventListener('change', function(){
-            var filePath = this.value;
-            var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
-            if(!allowedExtensions.exec(filePath)){
-                alert('Extensión no permitida. Utiliza: .jpeg/.jpg/.png/.gif.');
-                fileInput.value = '';
-                return false;
-            }else{
-                alert('Extensión correcta.');
-                return true;
-            }
-        })
-        price.onBlur = () => {
-            this.alert('Este campo es obligatorio');
+        else if (price.value.length > 6) {
+            price.alert('El precio del producto es mayor a 6 cifras, si esto es correcto contacta al administrador');
+            errors.price = ('El precio del producto es mayor a 6 cifras, si esto es correcto contacta al administrador');
         }
-    
-        btnSubmit.addEventListener('click', function(event){
-            event.preventDefault();
-            let errors = {}
-            if(productName.value.length < 5){
-                productName.alert('El nombre debe tener al menos 5 carácteres');
-                errors.productName = 'El nombre debe tener al menos 5 carácteres';
-            }
-            else if(shortDescription.value.length < 10){
-                shortDescription.alert('Este campo debe tener mínimo 10 carácteres');
-                errors.shortDescription = ('Este campo debe tener mínimo 10 carácteres');
-            }
-            else if(longDescription.value.length < 20){
-                longDescription.alert('Este campo debe tener mínimo 20 carácteres');
-                errors.longDescription = ('Este campo debe tener mínimo 20 carácteres');
-            }
-            else if(price.value.length > 6){
-                price.alert('El precio del producto es mayor a 6 cifras, si esto es correcto contacta al administrador');
-                errors.price = ('El precio del producto es mayor a 6 cifras, si esto es correcto contacta al administrador');
-            }
-            else{
-                formEdit.submit();
-            }
-        })
-    });
-    
-})
+        else {
+            formEdit.submit();
+        }
+    })
+});
--- Mi-Trebol/public/js/validateEditFrontEnd.js
+++ Mi-Trebol/public/js/validateEditFrontEnd.js
@@ -1,68 +1,104 @@
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     let formEdit = document.querySelector('#formEdit');
     let productName = document.querySelector('#text');
     let shortDescription = document.querySelector('#text2');
     let longDescription = document.querySelector('#text3');
     let imgMod = document.querySelector('#imgMod');
     let price = document.querySelector('#price');
-    let btnSubmit = document.querySelector('#submitAdd');
-    console.log(imgMod);
-    console.log(shortDescription);
-    console.log(btnSubmit);
+    let btnSubmit = document.querySelector('#submitEdit');
+    let smallNameTag = document.querySelector('.smallErName small');
+    let smallShortTag = document.querySelector('.erShortDes');
+    let smallLongTag = document.querySelector('.erLongDes');
+    let smallImage = document.querySelector('.erImage');
+    let smallPrice = document.querySelector('.erPrice');
+    var errors = [];
 
-    productName.addEventListener('blur', function(){
-    alert('Este campo es obligatorio');
+    productName.addEventListener('change', function (){
+        if(productName.value == ""){
+            errors.push('Es necesario agregar el nombre del producto');
+            errors.forEach(error => {
+                if(error == 'Es necesario agregar el nombre del producto'){
+                   smallNameTag.innerHTML += `${error}`
+                }
+            })
+            //alert('Es necesario agregar el nombre del producto');
+        };
     })
-    shortDescription.onBlur = () => {
-        this.alert('Este campo es obligatorio');
-    }
-    longDescription.onBlur = () => {
-        this.alert('Este campo es obligatorio');
-    }
-    imgMod.onBlur = () => {
-        this.alert('Es necesario añadir una imagen');
-    }
-    imgMod.addEventListener('change', function(){
+
+    shortDescription.addEventListener('change', function(){
+        if(shortDescription.value == ""){
+            errors.push('Es necesario escribir una descripción corta');
+            errors.forEach(error => {
+                if(error == 'Es necesario escribir una descripción corta'){
+                   smallShortTag.innerHTML += `${error}`
+                    alert('Es necesario escribir una descripción corta');
+                }
+            })
+        }
+    })
+
+    longDescription.addEventListener('change', function(){
+        if(longDescription.value == ""){
+            errors.push('Agrega una descripción detallada del producto');
+            errors.forEach(error => {
+                if(error == 'Agrega una descripción detallada del producto'){
+                   smallLongTag.innerHTML += `${error}`
+                    alert('Agrega una descripción detallada del producto');
+                }
+            })
+        }
+    })
+
+    imgMod.addEventListener('change', function () {
         var filePath = this.value;
-        var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
-        if(!allowedExtensions.exec(filePath)){
-            alert('Extensión no permitida. Utiliza: .jpeg/.jpg/.png/.gif.');
+        var allowedExtensions = /(.jpg||.jpeg||.png||.gif)$/i;
+        if (!allowedExtensions.exec(filePath)) {
+            errors.push('Extensión no permitida. Utiliza: .jpeg/.jpg/.png/.gif.');
+            errors.forEach(error => {
+                if(error == 'Extensión no permitida. Utiliza: .jpeg/.jpg/.png/.gif.'){
+                   smallImage.innerHTML += `${error}`
+                    alert('Agrega una descripción detallada del producto');
+                }
+            })
             fileInput.value = '';
             return false;
-        }else{
-            alert('Extensión correcta.');
-            return true;
-        }
+        } 
+        //else {  alert('Extensión correcta.');
+        //  return true; }
     })
-    price.onBlur = () => {
-        this.alert('Este campo es obligatorio');
-    }
 
-    btnSubmit.addEventListener('click', function(event){
-        event.preventDefault();
-        let errors = {}
-        if(productName.value.length < 5){
-            productName.alert('El nombre debe tener al menos 5 carácteres');
+    price.addEventListener('blur', function(){
+        errors.push('Debe asignar el precio');
+        errors.forEach(error => {
+            if(error == 'Debe asignar el precio'){
+               smallPrice.innerHTML += `${error}`
+                alert('Debe asignar el precio');
+            }
+        })
+    })
+
+    btnSubmit.addEventListener('click', function (evento) {
+        evento.preventDefault();
+        
+        if (productName.value.length < 5) {
+            alert('El nombre debe tener al menos 5 carácteres');
             errors.productName = 'El nombre debe tener al menos 5 carácteres';
         }
-        else if(shortDescription.value.length < 10){
-            shortDescription.alert('Este campo debe tener mínimo 10 carácteres');
-            errors.shortDescription = ('Este campo debe tener mínimo 10 carácteres');
+        else if (shortDescription.value.length < 10) {
+            alert('La descripción corta debe tener mínimo 10 carácteres');
+            errors.shortDescription = ('La descripción corta debe tener mínimo 10 carácteres');
         }
-        else if(longDescription.value.length < 20){
-            longDescription.alert('Este campo debe tener mínimo 20 carácteres');
-            errors.longDescription = ('Este campo debe tener mínimo 20 carácteres');
+        else if (longDescription.value.length < 20) {
+            alert('La descripción larga debe tener mínimo 20 carácteres');
+            errors.longDescription = ('La descripción larga debe tener mínimo 20 carácteres');
         }
-        else if(price.value.length > 6){
-            price.alert('El precio del producto es mayor a 6 cifras, si esto es correcto contacta al administrador');
+        else if (price.value.length > 6) {
+            alert('El precio del producto es mayor a 6 cifras, si esto es correcto contacta al administrador');
             errors.price = ('El precio del producto es mayor a 6 cifras, si esto es correcto contacta al administrador');
         }
-        else{
+        else {
             formEdit.submit();
         }
     })
 });
-
-// faltan validaciones de imagenes (tamaño y tipo de archivo).
-// probar!
