b5e6eb64041054716580e5f96820dfad5da1b7d5
Daniel GM <danielgarciamendoza.204@gmail.com>
2022-03-22 00:05:38 -0600

    Proyecto react creado

---
+++ Admin-Dashboard/src/views/404/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FourFour = () => {
+  return (
+    <>
+      <h2> Hola</h2>
+      <div> Pagina 404 Hubo un error o la ruta no esta establecida</div>
+
+    </>
+  );
+};
+
+export default FourFour;
---
+++ Admin-Dashboard/src/views/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h2> Hola</h2>
+      <div> Espacio</div>
+    </>
+  );
+};
+
+export default Home;
